bf1fe60d25b241764678266522df4a53d03bc974
MarivaldoProgramador <Pedromarivaldo10@gmail.com>
2023-07-09 21:34:01 -0300

    Update clima.py

--- clima.py
+++ clima.py
@@ -1,26 +1,26 @@
 import requests
 import PySimpleGUI as sg
-from chave import key
-
+import os
 
 def temperaturas():
     try:
-        api_key = key
-        site = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_br"
+        key = os.getenv('api_key')
+        site = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={key}&lang=pt_br"
 
         requisicao = requests.get(site)
         requisicao_dicionario = requisicao.json()
 
         situacao = requisicao_dicionario['weather'][0]['description']
         temperatura = requisicao_dicionario['main']['temp'] - 273.15
-        sensacao = requisicao_dicionario['main']['feels_like'] - 273.15   
+        sensacao = requisicao_dicionario['main']['feels_like'] - 273.15    
     except:
         janela['mensagem'].update("""Houve um erro! 
-Verifique se as informaçoes estão digitadas corretamente.""")  
+Verifique se as informaçoes estão digitadas corretamente.""")    
     else:
         janela['mensagem'].update(f"""Situação: {situacao.upper()}
 Temperatura: {temperatura:.1f}°C
 Sensação térmica: {sensacao:.1f}°C""")
+
 
 sg.theme("SystemDefault")
 layout = [
@@ -35,7 +35,7 @@ janela = sg.Window("CLIMA", layout)
 while True:
     eventos, valores = janela.read()
     if eventos == sg.WINDOW_CLOSED:
-        break    
+        break   
     elif eventos == "Buscar":
         cidade = valores['cidade']
         temperaturas()
